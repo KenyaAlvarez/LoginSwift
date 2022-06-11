f765c65d629fcade9c3c7f12761993263af07e13
Arturo Sánchez <96492801+KenyaKateAlvarezBaeza@users.noreply.github.com>
2022-06-11 18:11:33 -0500

    Loggin
    Project setup

---
+++ login-test/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  login-test
+//
+//  Created by Arturo Sanchez on 27/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    var body: some View {
+        NavigationView{
+            ZStack{
+                Color.indigo
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                
+                VStack{
+                    Text("login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login"){
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.indigo)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are loged in @\(username)"), isActive: $showingLoginScreen){
+                        EmptyView()
+                    }
+                    
+                }
+            }
+            .navigationBarHidden(true)
+        }
+    }
+    
+    func authenticateUser (username: String, password: String){
+        if username.lowercased() == "kenya"{
+            wrongUsername = 0
+            if password.lowercased() == "kenya1511"{
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ login-test/login_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  login_testApp.swift
+//  login-test
+//
+//  Created by Arturo Sanchez on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct login_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
